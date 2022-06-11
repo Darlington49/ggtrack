0d0a50f35b474860f4da4a5f5183f82278ba6f85
Youcef <youce.31@gmail.com>
2022-06-12 00:34:19 +0100

    trouble shooting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://test.mosquitto.org");
 var count = 0;
-require("dotenv").config();
+// require("dotenv").config();
 // const Payload = require("Payload");
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT 
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
