ce72e344df5d31a65618586cc6bff1962837ee2b
Youcef <youce.31@gmail.com>
2022-06-12 00:16:51 +0100

    Add web server

--- mqtt.js
+++ mqtt.js
@@ -4,6 +4,18 @@ var count = 0;
 require("dotenv").config();
 // const Payload = require("Payload");
 
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 const mongoose = require("mongoose");
 const { Payload } = require("./Payload");
 const connect = mongoose
